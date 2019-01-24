0f14eb56917ed88da83af635900a3327c112ff4d
hamza0867 <hamza0867@gmail.com>
2019-01-24 23:36:19 +0100

    Add pg and rails_12factor to the profuction group

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'repos/index'
+  get 'greeter/hello'
+  get 'greeter/goodbye'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'repos#index'
 end
